2964fe169374d17fe93fedbb06a7d87031ca639f
Algunenano <algunenano@gmail.com>
2015-12-08 20:14:23 +0100

    Enable debug by default

--- myCppEnv.sh
+++ myCppEnv.sh
@@ -60,7 +60,7 @@ echo 'cmake_minimum_required(VERSION 3.1.0 FATAL_ERROR)
 project('$fileName')
 
 set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Wextra -Weffc++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wpedantic -Wextra -Weffc++ -g")
 
 include_directories(include)
 file(GLOB '$upperCaseName'_SF "src/*")
